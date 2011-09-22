269ff43c882c5005510fb90f8ffa13b342a352d0
Valdas Žemaitis <valdas@idega.com>
2011-09-22 19:45:18 +0300

    Suggested volunteers are loaded by assignment type(s)

--- src/java/com/idega/volunteer/business/VolunteerServices.java
+++ src/java/com/idega/volunteer/business/VolunteerServices.java
@@ -1,17 +1,21 @@
 package com.idega.volunteer.business;
 
+import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Level;
 
 import javax.ejb.FinderException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
@@ -22,18 +26,29 @@ import com.idega.company.event.CompanyCreatedEvent;
 import com.idega.core.business.DefaultSpringBean;
 import com.idega.event.UserCreatedEvent;
 import com.idega.idegaweb.IWResourceBundle;
+import com.idega.jbpm.bean.VariableInstanceInfo;
+import com.idega.jbpm.data.VariableInstanceQuerier;
+import com.idega.jbpm.data.dao.BPMDAO;
 import com.idega.user.business.GroupBusiness;
 import com.idega.user.business.UserBusiness;
 import com.idega.user.data.Group;
 import com.idega.user.data.User;
+import com.idega.util.CoreConstants;
 import com.idega.util.IWTimestamp;
 import com.idega.util.ListUtil;
 import com.idega.util.StringUtil;
+import com.idega.util.text.Item;
 import com.idega.volunteer.VolunteerConstants;
 
 @Service("volunteerServices")
 @Scope(BeanDefinition.SCOPE_SINGLETON)
 public class VolunteerServices extends DefaultSpringBean implements ApplicationListener {
+	
+	@Autowired
+	private VariableInstanceQuerier querier;
+	
+	@Autowired
+	private BPMDAO bpmDAO;
 	
 	public Map<Locale, Map<String, String>> getVolunteers() {
 		Map<Locale, Map<String, String>> allVolunteers = new HashMap<Locale, Map<String,String>>();
@@ -81,18 +96,96 @@ public class VolunteerServices extends DefaultSpringBean implements ApplicationL
 		return Collections.emptyList();
 	}
 	
-	public Map<Locale, Map<String, String>> getSuggestedVolunteers() {
-		Map<Locale, Map<String, String>> allVolunteers = new HashMap<Locale, Map<String,String>>();
+	public List<Item> getSuggestedVolunteers(String types) {
+		List<Item> volunteers = new ArrayList<Item>();
 		
-		//	TODO:	use real suggested volunteers
-		Map<String, String> volunteers = new HashMap<String, String>();
-		allVolunteers.put(getCurrentLocale(), volunteers);
+		List<Long> piIds = bpmDAO.getProcessInstanceIdsByProcessDefinitionNames(Arrays.asList("VolunteerAccount"));
+		if (ListUtil.isEmpty(piIds))
+			return volunteers;
 		
-		volunteers.put("1", "Suggessted Volunter_1");
-		volunteers.put("2", "Suggested Volunteer_2");
-		volunteers.put("3", "Suggested Volunteer_3");
+		Collection<VariableInstanceInfo> vars = querier.getVariablesByProcessInstanceIdAndVariablesNames(Arrays.asList(
+				"string_volunteerAccountPersonalId",
+				"string_volunteerAccountRegistered",
+				"string_volunteerAccountAvailable",
+				"list_volunteerAccountAreaOfInterests"
+			), piIds, false, false, false);
+		if (ListUtil.isEmpty(vars))
+			return volunteers;
 		
-		return allVolunteers;
+		Map<Long, List<VariableInstanceInfo>> groupedVars = querier.getGroupedVariables(vars);
+		if (groupedVars == null)
+			return volunteers;
+		
+		Map<Long, Collection<?>> interests = new HashMap<Long, Collection<?>>();
+		Map<Long, String> availableVolunteers = new HashMap<Long, String>();
+		for (Long piId: groupedVars.keySet()) {
+			List<VariableInstanceInfo> variables = groupedVars.get(piId);
+			if (ListUtil.isEmpty(variables))
+				continue;
+			
+			for (VariableInstanceInfo var: variables) {
+				String name = var.getName();
+				if (StringUtil.isEmpty(name))
+					continue;
+				
+				Serializable value = var.getValue();
+				if (value == null || StringUtil.isEmpty(value.toString()))
+					continue;
+				
+				if (name.equals("string_volunteerAccountPersonalId")) {
+					availableVolunteers.put(piId, value.toString());
+				} else if (name.equals("string_volunteerAccountRegistered") && !Boolean.valueOf(value.toString())) {
+					availableVolunteers.remove(piId);
+					interests.remove(piId);
+				} else if (name.equals("string_volunteerAccountAvailable") && !Boolean.valueOf(value.toString())) {
+					availableVolunteers.remove(piId);
+					interests.remove(piId);
+				} else if (name.equals("list_volunteerAccountAreaOfInterests")) {
+					if (value instanceof Collection<?>)
+						interests.put(piId, (Collection<?>) value);
+				}
+			}
+		}
+		if (availableVolunteers == null || availableVolunteers.isEmpty())
+			return volunteers;
+		
+		List<String> suggestedVolunteers = new ArrayList<String>();
+		List<String> assignmentTypes = StringUtil.isEmpty(types) ? new ArrayList<String>(0) : Arrays.asList(types.split(CoreConstants.SPACE));
+		for (Long piId: availableVolunteers.keySet()) {
+			String personalId = availableVolunteers.get(piId);
+			if (ListUtil.isEmpty(assignmentTypes)) {
+				suggestedVolunteers.add(personalId);
+			} else {
+				Collection<?> preferences = interests.get(piId);
+				if (ListUtil.isEmpty(preferences))
+					continue;
+				else {
+					for (Object preference: preferences) {
+						if (assignmentTypes.contains(preference.toString()) && !suggestedVolunteers.contains(personalId))
+							suggestedVolunteers.add(personalId);
+					}
+				}
+			}
+		}
+		
+		if (ListUtil.isEmpty(suggestedVolunteers))
+			return volunteers;
+		
+		UserBusiness userBusiness = getServiceInstance(UserBusiness.class);
+		for (String personalId: suggestedVolunteers) {
+			User user = null;
+			try {
+				user = userBusiness.getUser(personalId);
+			} catch (Exception e) {
+				getLogger().log(Level.WARNING, "Error getting user by personal ID: " + personalId, e);
+			}
+			if (user == null)
+				continue;
+			
+			volunteers.add(new Item(user.getId(), user.getName()));
+		}
+		
+		return volunteers;
 	}
 	
 	public void addUserToGroup(String groupName, User user, String groupNameToRemoveFrom) {
